3de910a499547945ac2e779bd090331b9e113089
Akihide Ito <akky.supernova@gmail.com>
2022-03-29 17:40:12 +0900

    refactor: remove 42 credentials file

--- src/credentials.ts
+++ src/credentials.ts
@@ -1,33 +1,19 @@
-type CredentialsTokyo42 = {
-  name: string;
-  password: string;
-};
-
 type CredentialsDiscord = {
   email: string;
   password: string;
 };
 
 type Credentials = {
-  tokyo42: CredentialsTokyo42;
   discord: CredentialsDiscord;
 };
 
 const setCredentials = (envVars: NodeJS.ProcessEnv) => {
-  if (!envVars.TOKYO_42_USERNAME)
-    throw new Error("TOKYO_42_USERNAME is not defined");
-  if (!envVars.TOKYO_42_PASSWORD)
-    throw new Error("TOKYO_42_PASSWORD is not defined");
   if (!envVars.DISCORD_EMAIL)
-    throw new Error("DISCORD_PASSWORD is not defined");
+    throw new Error("DISCORD_PASSWORD is not defined. create .env file");
   if (!envVars.DISCORD_PASSWORD)
-    throw new Error("DISCORD_PASSWORD is not defined");
+    throw new Error("DISCORD_PASSWORD is not defined. create .env file");
 
   const credentials: Credentials = {
-    tokyo42: {
-      name: envVars.TOKYO_42_USERNAME,
-      password: envVars.TOKYO_42_PASSWORD,
-    },
     discord: {
       email: envVars.DISCORD_EMAIL,
       password: envVars.DISCORD_PASSWORD,
@@ -37,4 +23,4 @@ const setCredentials = (envVars: NodeJS.ProcessEnv) => {
   return credentials;
 };
 
-export { setCredentials, CredentialsTokyo42, CredentialsDiscord, Credentials };
+export { setCredentials, CredentialsDiscord, Credentials };
